d3b704f9b7a7c67d8ef4476341397c54790e6c2d
kokiy459876 <kouki.yamada@outlook.jp>
2024-07-31 00:51:15 +0900

    1st commit

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_007</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+        // 連想配列
+        $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 出力
+        print_r($personal_data);
+        ?>
+     </p>
+ </body>
+ </html>
